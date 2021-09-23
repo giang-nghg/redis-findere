1527c1f9c3ab1a1b011500327f9d3a8199e8dee4
Giang Nguyen <giang.nghg@gmail.com>
2021-09-23 21:15:25 +0100

    Integrate findere

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,19 @@
 FROM redis
 
 RUN apt update
-RUN apt install -y cmake
+RUN apt install -y cmake build-essential git zlib1g-dev
 
 WORKDIR /usr/src/app
 COPY . .
-RUN cmake .
+
+WORKDIR build
+RUN cmake ..
 RUN make
 
 RUN mkdir -p /usr/local/etc/redis/modules
 RUN cp libredis_findere.so /usr/local/etc/redis/modules/findere.so
 RUN chmod +x /usr/local/etc/redis/modules/findere.so
 
+WORKDIR ..
 COPY redis.conf /usr/local/etc/redis/redis.conf
 CMD [ "redis-server", "/usr/local/etc/redis/redis.conf" ]
--- library.cpp
+++ library.cpp
@@ -1,10 +1,21 @@
 #include "redismodule.h"
 #include <stdlib.h>
 
+#include "findere/src/libraries/findere/customAMQ.hpp"
+
+
+class bfAMQ : public customAMQ {};
+
+
 int HelloworldRand_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
     RedisModule_ReplyWithLongLong(ctx,rand());
     return REDISMODULE_OK;
 }
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
     if (RedisModule_Init(ctx,"helloworld",1,REDISMODULE_APIVER_1)
@@ -17,3 +28,7 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) 
 
     return REDISMODULE_OK;
 }
+
+#ifdef __cplusplus
+}
+#endif
